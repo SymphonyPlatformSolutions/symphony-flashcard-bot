421ad0652597b2c2d4db8814d0c9552eafa157c5
Edwin <nyg.edwin@gmail.com>
2019-10-11 16:41:51 +0800

    ROOM1 Change Oct2019

--- python/listeners/message_processor.py
+++ python/listeners/message_processor.py
@@ -4,7 +4,9 @@ from sym_api_client_python.processors.sym_message_parser import SymMessageParser
 class MessageProcessor:
     def __init__(self, bot_client):
         self.bot_client = bot_client
+        self.message_client = self.bot_client.get_message_client()
         self.message_parser = SymMessageParser()
+        self.help_message = 'Input Correct'
 
     def process(self, msg):
         msg_text = self.message_parser.get_text(msg)
@@ -17,3 +19,34 @@ class MessageProcessor:
             stream_id = self.message_parser.get_stream_id(msg)
             self.bot_client.get_message_client(). \
                 send_msg(stream_id, msg_to_send)
+
+    def processROOM(self, msg):
+        stream_id = self.message_parser.get_stream_id(msg)
+        msg_text = self.message_parser.get_text(msg)
+        command = msg_text[0]
+        
+        if (command == '/fundname') or (command == '/ISIN'):
+            self.send_message(stream_id, self.help_message)
+        
+        else: 
+            self.send_message(stream_id, 'We only support the functions /FUNDNAME or /ISIN')
+        
+        # msg_to_send = dict (
+        # message = f'<messageML>{error_msg}</messageML>'
+        # )
+
+        # if msg_text:
+        # stream_id = self.message_parser.get_stream_id(msg)
+        # self.bot_client.get_message_client(). \
+        # send_msg(stream_id, msg_to_send)
+
+
+
+        # if (msg_text == '/fundname') or (msg_text == '/ISIN'):
+        #     error_msg = 'Ok!'
+        
+        # else: 
+        #     None
+        
+    def send_message(self, stream_id, msg_text):
+        self.message_client.send_msg(stream_id, dict(message=f'<messageML>{msg_text}</messageML>'))
