87fbeaddb81589e7a8e0bf5641744518330754ee
Yong Sheng Tan <yong.tan@symphony.com>
2019-10-17 10:32:24 +0800

    Fixed command processing

--- python/listeners/message_processor.py
+++ python/listeners/message_processor.py
@@ -4,58 +4,51 @@ from sym_api_client_python.processors.sym_message_parser import SymMessageParser
 class MessageProcessor:
     def __init__(self, bot_client):
         self.bot_client = bot_client
-        self.message_client=self.bot_client.get_message_client()
+        self.message_client = self.bot_client.get_message_client()
         self.message_parser = SymMessageParser()
-        self.help_message='Welcome to MI Flash Bot'
+        self.help_message = 'Welcome to MI Flash Bot'
 
-    def process(self, msg):
+    def parse_message(self, msg):
+        stream_id = self.message_parser.get_stream_id(msg)
         msg_text = self.message_parser.get_text(msg)
-        template = '<messageML>Hello {}, hope you are doing well!You love BOS right?</messageML>'
-        msg_to_send = dict(
-            message=template.format(self.message_parser.get_im_first_name(msg))
-        )
+        command = msg_text[0].lower()
+        return stream_id, msg_text, command
 
-        if msg_text:
-            stream_id = self.message_parser.get_stream_id(msg)
-            self.bot_client.get_message_client(). \
-                send_msg(stream_id, msg_to_send)
+    def send_message(self, stream_id, msg_text):
+        self.message_client.send_msg(stream_id, dict(message=f'<messageML>{msg_text}</messageML>'))
 
-    def processIM(self, msg):
-        stream_id=self.message_parser.get_stream_id(msg)
-        msg_text=self.message_parser.get_text(msg)
-        command = msg_text[0]
-    
+    def processROOM(self, msg):
+        stream_id, msg_text, command = self.parse_message(msg)
+
         if command == '/help':
             print('doing help')
-            self.send_message(stream_id,self.help_message)
+            self.send_message(stream_id, self.help_message)
 
         elif command == '/upload':
             print('doing upload')
-
-            self.send_message(stream_id,'upload')
+            self.send_message(stream_id, 'upload')
 
         elif command == '/download':
             print('doing download')
-            self.send_message(stream_id,'download')
+            self.send_message(stream_id, 'download')
 
         elif command == '/blast':
             print('doing blast')
-            self.send_message(stream_id,'blast')
+            self.send_message(stream_id, 'blast')
 
         else:
-            self.send_message(stream_id,'Sorry, I do not understand')
+            self.send_message(stream_id, 'Sorry, I do not understand')
 
-    def processROOM(self, msg):
-        stream_id = self.message_parser.get_stream_id(msg)
-        msg_text = self.message_parser.get_text(msg)
-        command = msg_text[0]
+    def processIM(self, msg):
+        stream_id, msg_text, command = self.parse_message(msg)
 
-        if (command == '/fundname') or (command == '/ISIN'):
-            self.send_message(stream_id, self.help_message)
+        if command == '/isin':
+            print('doing isin')
+            self.send_message(stream_id, 'isin')
 
-        else: 
-            self.send_message(stream_id, 'We only support the functions /FUNDNAME or /ISIN')
+        elif command == '/fundname':
+            print('doing fundname')
+            self.send_message(stream_id, 'fundname')
 
-    def send_message(self,stream_id,msg_text):
-        self.message_client.send_msg(stream_id,dict(message=f'<messageML>{msg_text}</messageML>'))
-
+        else:
+            self.send_message(stream_id, 'We only support the functions /fundname or /isin')
