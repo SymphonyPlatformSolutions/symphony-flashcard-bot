949e194bca7ab00c4c6127658d10cc768131fd28
Yong Sheng Tan <yong.tan@symphony.com>
2019-10-17 13:33:58 +0800

    Completed blast messages

--- python/processors/admin_processor.py
+++ python/processors/admin_processor.py
@@ -11,4 +11,12 @@ class AdminProcessor:
         self.bot_client.get_message_client().send_msg(stream_id, dict(message=f'<messageML>{msg_text}</messageML>'))
 
     def blast_messages(self, file, message):
-        self.send_message(file, message)
+        emails = file.splitlines()
+        for email in emails:
+            user = self.bot_client.get_user_client().get_user_from_email(email, True)
+            if len(user) == 0:
+                print(f'Cannot find user with email {email}')
+                continue
+            user_id = user['id']
+            stream_id = self.bot_client.get_stream_client().create_im([ user_id ])['id']
+            self.send_message(stream_id, message)
